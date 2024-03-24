18fbe5aea4d088d292e8e1d224baf02fe7841e6d
Fingsinz <fingsinz@outlook.com>
2024-03-24 17:28:45 +0800

    finished Day2

--- Day2/client.cpp
+++ Day2/client.cpp
@@ -2,19 +2,14 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <iostream>
-
-void errorif(bool condition, const char *errmsg)
-{
-	if (condition)
-	{
-		perror(errmsg);
-		exit(EXIT_FAILURE);
-	}
-}
+#include <unistd.h>
+#include "util.h"
 
 int main()
 {
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	errorif(sockfd == -1, "socket create error");
 
 	struct sockaddr_in serverAddr;
 	bzero(&serverAddr, sizeof(serverAddr));
@@ -23,7 +18,42 @@ int main()
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddr.sin_port = htons(1234);
 
-	connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	errorif(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1, "socket connect error");
 
+	while (true)
+	{
+		char buffer[1024];
+		std::cin >> buffer;
+		size_t writeLen = write(sockfd, buffer, sizeof(buffer));
+
+		if (writeLen == -1)
+		{
+			std::cout << "Socket already disconnected!\n";
+			break;
+		}
+
+		bzero(buffer, sizeof(buffer));
+
+		size_t readLen = read(sockfd, buffer, sizeof(buffer));
+
+		if (readLen > 0)
+		{
+			std::cout << buffer << "\n";
+		}
+
+		else if (readLen == 0)
+		{
+			std::cout << "Server socket disconnected!\n";
+			break;
+		}
+
+		else if (readLen == -1)
+		{
+			close(sockfd);
+			errorif(true, "socket read error");
+		}
+	}
+
+	close(sockfd);
 	return 0;
 }
--- Day2/server.cpp
+++ Day2/server.cpp
@@ -2,19 +2,13 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
-void errorif(bool condition, const char *errmsg)
-{
-	if (condition)
-	{
-		perror(errmsg);
-		exit(EXIT_FAILURE);
-	}
-}
+#include <unistd.h>
+#include "util.h"
 
 int main()
 {
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	errorif(sockfd == -1, "socket create error");
 
 	struct sockaddr_in serverAddr;
 	bzero(&serverAddr, sizeof(serverAddr));
@@ -23,21 +17,48 @@ int main()
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddr.sin_port = htons(1234);
 
-	bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	errorif(bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1, "socket bind error");
 
-	while (1)
+	errorif(listen(sockfd, SOMAXCONN) == -1, "socket listen error");
+
+	struct sockaddr_in clientAddr;
+	socklen_t clientAddrLen = sizeof(clientAddr);
+	bzero(&clientAddr, sizeof(clientAddr));
+	int clientfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
+
+	errorif(clientfd == -1, "socket accept error");
+
+	std::cout << "Client connected: " << clientfd <<
+		"\tIP: " << inet_ntoa(clientAddr.sin_addr) <<
+		"\tPort: " << ntohs(clientAddr.sin_port) << std::endl;
+
+	while (true)
 	{
-		listen(sockfd, SOMAXCONN);
+		char buffer[1024];
+		bzero(buffer, sizeof(buffer));
 
-		struct sockaddr_in clientAddr;
-		socklen_t clientAddrLen = sizeof(clientAddr);
-		bzero(&clientAddr, sizeof(clientAddr));
-		int clientfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
+		size_t readLen = read(clientfd, buffer, sizeof(buffer));
 
-		std::cout << "Client connected: " << clientfd <<
-			"\tIP: " << inet_ntoa(clientAddr.sin_addr) <<
-			"\tPort: " << ntohs(clientAddr.sin_port) << std::endl;
+		if (readLen > 0)
+		{
+			std::cout << "Message from client:" << clientfd << ": " << buffer << "\n";
+			write(clientfd, buffer, sizeof(buffer));
+		}
+
+		else if (readLen == 0)
+		{
+			std::cout << "Client " << clientfd << " disconnected\n";
+			close(clientfd);
+			break;
+		}
+
+		else if (readLen == -1)
+		{
+			close(clientfd);
+			errorif(true, "socket read error");
+		}
 	}
 
+	close(sockfd);
 	return 0;
 }
