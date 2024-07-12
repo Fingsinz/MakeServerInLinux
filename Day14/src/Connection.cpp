6e38d1c12b19ee66146c482592d3de9d4340ce07
Fingsinz <fingsinz@outlook.com>
2024-07-12 18:43:35 +0800

    updated Connection类

--- Day14/src/Connection.cpp
+++ Day14/src/Connection.cpp
@@ -3,206 +3,144 @@
 #include "Channel.h"
 #include "Socket.h"
 #include "util.h"
+#include <Common.h>
 #include <cassert>
 #include <cstring>
 #include <iostream>
 #include <unistd.h>
-#include <Common.h>
 
-Connection::Connection(EventLoop *loop, int fd) {
-    mSocket = std::make_unique<Socket>();
-    mSocket->setFd(fd);
-
+Connection::Connection(EventLoop *loop, int fd, int id)
+        : mConnfd(fd)
+        , mConnId(id)
+        , mLoop(loop) {
     if (loop != nullptr) {
-        mChannel = std::make_unique<Channel>(loop, fd);
+        mChannel = std::make_unique<Channel>(mConnfd, loop);
         mChannel->useET();
+        mChannel->setReadCallback(std::bind(&Connection::handleMessage, this));
         mChannel->enableRead();
     }
-    mReadBuffer = std::make_unique<Buffer>();
-    mSendBuffer = std::make_unique<Buffer>();
-
-    mState = State::Connected;
+    mReadBuf = std::make_unique<Buffer>();
+    mSendBuf = std::make_unique<Buffer>();
+}
+Connection::~Connection() {
+    ::close(mConnfd);
 }
 
-Connection::~Connection() {}
-
 FLAG Connection::read() {
-    if (mState != State::Connected) {
-        errorif(true, "[Error]\tConnection state is disconnected!");
-        return FLAG::FL_CONNECTION_ERROR;
-    }
-    assert(mState == State::Connected and "[Error]\tConnection state is disconnected!");
-
-    mReadBuffer->clear();
-    if (mSocket->isNonBlocking())
-        return readNonBlocking();
-    else
-        return readBlocking();
+    mReadBuf->clear();
+    readNonBlocking();
 }
 
 FLAG Connection::write() {
-    if (mState != State::Connected) {
-        errorif(true, "[Error]\tConnection state is disconnected!");
-        return FLAG::FL_CONNECTION_ERROR;
-    }
-    FLAG flag = FLAG::FL_UNDIFINED;
-
-    if (mSocket->isNonBlocking())
-        flag = writeNonBlocking();
-    else
-        flag = writeBlocking();
-
-    mSendBuffer->clear();
-    return flag;
+    writeNonBlocking();
+    mSendBuf->clear();
 }
 
 FLAG Connection::send(std::string msg) {
-    setSentBuffer(msg.c_str());
+    setSentBuf(msg.c_str());
     write();
-    return FLAG::FL_SUCCESS;
+    return FL_SUCCESS;
 }
 
-void Connection::setDeleteConnectionCallback(std::function<void(int)> const &callback) {
-    mDeleteConnectionCallback = std::move(callback);
+FLAG Connection::sent(char const *msg) {
+    setSentBuf(msg);
+    write();
 }
 
 void Connection::setOnMessageCallback(std::function<void(Connection *)> const &callback) {
-    mOnMessageCallback = std::move(callback);
-    std::function<void()> busi = std::bind(&Connection::business, this);
-    mChannel->setReadCallback(busi);
+    onMessageCb = std::move(callback);
 }
 
-void Connection::business() {
+void Connection::setOnCloseCallback(std::function<void(int)> const &callback) {
+    onCloseCb = std::move(callback);
+}
+
+void Connection::setSentBuf(char const *str) {
+    mSendBuf->setBuf(str);
+}
+
+Buffer *Connection::getReadBuf() {
+    return mReadBuf.get();
+}
+
+Buffer *Connection::getSentBuf() {
+    return mSendBuf.get();
+}
+
+void Connection::handleMessage() {
     read();
-    mOnMessageCallback(this);
+    if (onCloseCb) {
+        onMessageCb(this);
+    }
 }
 
-Connection::State Connection::getState() {
+void Connection::handleClose() {
+    if (mState != State::Disconnected) {
+        mState = State::Disconnected;
+        if (onCloseCb) {
+            onCloseCb(mConnId);
+        }
+    }
+}
+
+Connection::State Connection::getState() const {
     return mState;
 }
 
-void Connection::close() {
-    mDeleteConnectionCallback(mSocket->getFd());
+EventLoop *Connection::getLoop() const {
+    return mLoop;
 }
 
-void Connection::setSentBuffer(char const *str) {
-    mSendBuffer->setBuf(str);
+int Connection::getFd() const {
+    return mConnfd;
 }
 
-Buffer *Connection::getReadBuffer() {
-    return mReadBuffer.get();
+int Connection::getId() const {
+    return mConnId;
 }
 
-Buffer *Connection::getSendBuffer() {
-    return mSendBuffer.get();
-}
-
-Socket *Connection::getSocket() {
-    return mSocket.get();
-}
-
-FLAG Connection::readNonBlocking() {
-    int sockfd = mSocket->getFd();
-    char buf[1024];
-
+void Connection::readNonBlocking() {
+    char buf[1026];
     while (true) {
-        bzero(buf, sizeof(buf));
-        ssize_t readLen = ::read(sockfd, buf, sizeof(buf));
+        memset(buf, 0, sizeof(buf));
+        ssize_t bytesRead = ::read(mConnfd, buf, sizeof(buf));
 
-        if (readLen > 0)
-            mReadBuffer->append(buf, readLen);
-
-        // 客户端正常中断、继续读取
-        else if (readLen == -1 and errno == EINTR) {
-            std::cout << "[Continue Reading]\n";
+        if (bytesRead > 0) {
+            mReadBuf->append(buf, bytesRead);
+        } else if (bytesRead == -1 and errno == EINTR) {
             continue;
-        }
-
-        // 非阻塞 IO，表示数据全部读取完毕
-        else if (readLen == -1 and ((errno == EAGAIN) or (errno == EWOULDBLOCK))) {
+        } else if (bytesRead == -1 and (errno == EAGAIN or errno == EWOULDBLOCK)) {
             break;
-        }
-
-        // EOF，表示客户端断开连接
-        else if (readLen == 0) {
-            std::cout << "[Client " << sockfd << " disconnected]\n";
-            mState = State::Closed;
-            // close();	// 启用这句会段错误
+        } else if (bytesRead == 0) {
+            handleClose();
             break;
-        }
-
-        else {
-            std::cout << "[Error]\tOther error on client fd: " << sockfd << "\n";
-            mState = State::Closed;
-            close();
+        } else {
+            handleClose();
             break;
         }
     }
-    return FLAG::FL_SUCCESS;
 }
 
-FLAG Connection::readBlocking() {
-    int sockfd = mSocket->getFd();
-    ssize_t dataSize = mSocket->recvBufSize();
-    char buf[1024];
-    ssize_t readLen = ::read(sockfd, buf, sizeof(buf));
+void Connection::writeNonBlocking() {
+    char buf[mSendBuf->size()];
 
-    if (readLen > 0) {
-        mReadBuffer->append(buf, readLen);
-    }
+    memcpy(buf, mSendBuf->c_str(), mSendBuf->size());
 
-    // 客户端断开连接
-    else if (readLen == 0) {
-        std::cout << "[Client " << sockfd << " ]\tdisconnected\n";
-        mState = State::Closed;
-    }
-
-    else if (readLen == -1) {
-        std::cout << "[Error]\tOther error on client fd: " << sockfd << "\n";
-        mState = State::Closed;
-    }
-
-    return FLAG::FL_SUCCESS;
-}
-
-FLAG Connection::writeNonBlocking() {
-    int sockfd = mSocket->getFd();
-    char *buf = new char[mSendBuffer->size()];
-    memcpy(buf, mSendBuffer->c_str(), mSendBuffer->size());
-    int dataSize = mSendBuffer->size();
+    int dataSize = mSendBuf->size();
     int dataLeft = dataSize;
 
     while (dataLeft > 0) {
-        ssize_t writeLen = ::write(sockfd, buf + dataSize - dataLeft, dataLeft);
+        ssize_t bytesWrite = ::write(mConnfd, buf + dataSize - dataLeft, dataLeft);
 
-        if (writeLen == -1 and errno == EINTR) {
-            std::cout << "[Continue Writing]\n";
+        if (bytesWrite == -1 and errno == EINTR) {
             continue;
-        }
-
-        if (writeLen == -1 and errno == EAGAIN)
+        } else if (bytesWrite == -1 and errno == EAGAIN) {
             break;
-
-        if (writeLen == -1) {
-            std::cout << "[Error]\tOther error on client fd: " << sockfd << "\n";
-            mState = State::Closed;
+        } else if (bytesWrite == -1) {
+            handleClose();
             break;
+        } else {
+            dataLeft -= bytesWrite;
         }
-        dataLeft -= writeLen;
     }
-
-    delete[] buf;
-    return FLAG::FL_SUCCESS;
-}
-
-FLAG Connection::writeBlocking() {
-    // 没有处理 mSendBuffer 数据大于TCP写缓冲区的情况，可能会有bug
-    int sockfd = mSocket->getFd();
-    ssize_t writeLen = ::write(sockfd, mSendBuffer->c_str(), mSendBuffer->size());
-
-    if (writeLen == -1) {
-        std::cout << "[Error]\tOther error on client fd: " << sockfd << "\n";
-        mState = State::Closed;
-    }
-    return FLAG::FL_SUCCESS;
 }
--- Day14/src/include/Connection.h
+++ Day14/src/include/Connection.h
@@ -13,114 +13,82 @@ class Connection {
 public:
     enum State {
         Invalid = 0, // 初始无效状态
-        Handshaking, // 握手过程中的状态
         Connected,   // 连接建立
-        Closed,      // 连接关闭
-        Failed,      // 连接失败
+        Disconnected
     };
 
-private:
-    // EventLoop *mLoop;
-    // EventLoop指针
-    std::unique_ptr<Socket> mSocket;              // Socket指针
-    std::unique_ptr<Channel> mChannel{nullptr};   // Channel指针
-    std::unique_ptr<Buffer> mReadBuffer{nullptr}; // 读缓冲区
-    std::unique_ptr<Buffer> mSendBuffer{nullptr}; // 写缓冲区
-
-    State mState{Invalid};                                // 连接状态
-    std::function<void(int)> mDeleteConnectionCallback;   // 删除连接的回调函数
-    std::function<void(Connection *)> mOnMessageCallback; // 业务逻辑回调函数
-
-    FLAG readNonBlocking();  // 非阻塞读
-    FLAG writeNonBlocking(); // 非阻塞写
-    FLAG readBlocking();     // 阻塞读
-    FLAG writeBlocking();    // 阻塞写
-
 public:
-    explicit Connection(EventLoop *loop, int fd);
-    ~Connection();
-
     // 禁止拷贝和移动
     DISALLOW_COPY_AND_MOVE(Connection);
+    explicit Connection(EventLoop *loop, int fd, int id);
+    ~Connection();
 
     /**
-     * @brief 从与连接相关联的套接字读取数据
-     *
+     * @brief 读操作
      * @return FLAG 操作结果标记
      */
     FLAG read();
 
     /**
-     * @brief 将数据写入与该连接相关联的套接字
-     *
+     * @brief 写操作
      * @return FLAG 操作结果标记
      */
     FLAG write();
 
     /**
      * @brief 通过连接发送消息
-     *
      * @param msg 要发送的消息
      * @return FLAG 操作结果标记
      */
     FLAG send(std::string msg);
 
+    FLAG sent(char const *msg);
+
     /**
      * @brief 设置一个回调函数，在连接上接收到消息时调用
-     *
      * @param callback 回调函数
      */
     void setOnMessageCallback(std::function<void(Connection *)> const &callback);
 
     /**
-     * @brief 设置一个回调函数，在需要删除连接时调用
-     *
+     * @brief 设置一个回调函数，在需要断开连接时调用
      * @param callback 回调函数
      */
-    void setDeleteConnectionCallback(std::function<void(int)> const &callback);
+    void setOnCloseCallback(std::function<void(int)> const &callback);
 
-    /**
-     * @brief 与连接关联的业务逻辑
-     */
-    void business();
+    void setSentBuf(char const *str);
 
-    /**
-     * @brief 获取连接的状态
-     *
-     * @return 状态
-     */
-    State getState();
+    Buffer *getReadBuf();
 
-    /**
-     * @brief 关闭这个连接
-     */
-    void close();
+    Buffer *getSentBuf();
 
-    /**
-     * @brief 设置要通过连接发送的缓冲区
-     *
-     * @param str 要发送的缓冲区
-     */
-    void setSentBuffer(char const *str);
+    void handleMessage();
 
-    /**
-     * @brief 获取与该连接关联的读缓冲区
-     *
-     * @return 指向读缓冲区的指针
-     */
-    Buffer *getReadBuffer();
+    void handleClose();
 
-    /**
-     * @brief 获取与该连接关联的发送缓冲区
-     *
-     * @return 指向写缓冲区的指针
-     */
-    Buffer *getSendBuffer();
+    State getState() const;
 
-    /**
-     * @brief 获取与该连接关联的套接字
-     *
-     * @return Socket指针
-     */
-    Socket *getSocket();
+    EventLoop *getLoop() const;
+
+    int getFd() const;
+
+    int getId() const;
+
+private:
+    int mConnfd;
+    int mConnId;
+
+    State mState;
+
+    EventLoop *mLoop;
+
+    std::unique_ptr<Channel> mChannel;
+    std::unique_ptr<Buffer> mReadBuf;
+    std::unique_ptr<Buffer> mSendBuf;
+
+    std::function<void(int)> onCloseCb;
+    std::function<void(Connection *)> onMessageCb;
+
+    void readNonBlocking();
+    void writeNonBlocking();
 };
