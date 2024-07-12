da8010068436affcf8c4f768d87ead29beed2dcd
Fingsinz <fingsinz@outlook.com>
2024-07-12 17:37:21 +0800

    updated EventLoop类

---
+++ Day15/src/EventLoop.cpp
@@ -0,0 +1,29 @@
+﻿#include "EventLoop.h"
+#include "Channel.h"
+#include "Epoll.h"
+#include <vector>
+
+EventLoop::EventLoop() {
+    ep = std::make_unique<Epoll>();
+}
+
+EventLoop::~EventLoop() {}
+
+void EventLoop::loop() const {
+    while (true) {
+        // 使用epoll实例轮询事件
+        std::vector<Channel *> channels = ep->poll();
+
+        // 处理每个Channel的事件
+        for (Channel *channel : channels)
+            channel->handleEvent();
+    }
+}
+
+void EventLoop::updateChannel(Channel *channel) const {
+    ep->updateChannel(channel);
+}
+
+void EventLoop::deleteChannel(Channel *channel) const {
+    ep->deleteChannel(channel);
+}
