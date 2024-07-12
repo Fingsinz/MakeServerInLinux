929119cfc65645cf31ba51bb758886dab5c829a3
Fingsinz <fingsinz@outlook.com>
2024-07-12 18:11:05 +0800

    updated Acceptor类

---
+++ Day15/src/Acceptor.cpp
@@ -0,0 +1,77 @@
+﻿#include "Acceptor.h"
+#include "Channel.h"
+#include "EventLoop.h"
+#include "util.h"
+#include <arpa/inet.h>
+#include <cassert>
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+Acceptor::Acceptor(EventLoop *loop, std::string const &ip, int const port)
+        : mLoop(loop)
+        , mListenFd(-1) {
+    socketCreate();
+    socketBind(ip, port);
+    socketListen();
+
+    mChannel = std::make_unique<Channel>(loop, mListenFd);
+    std::function<void()> cb = std::bind(&Acceptor::acceptConnection, this);
+
+    mChannel->setReadCallback(cb);
+    mChannel->enableRead();
+}
+
+Acceptor::~Acceptor() {
+    mLoop->deleteChannel(mChannel.get());
+    ::close(mListenFd);
+}
+
+void Acceptor::socketCreate() {
+    assert(mListenFd == -1);
+    mListenFd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+    errorif(mListenFd == -1, "[Error]\t Socket Create failed");
+}
+
+void Acceptor::socketBind(std::string const &ip, int const port) {
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    addr.sin_port = htons(port);
+    if (::bind(mListenFd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        errorif(true, "[Error]\t Socket Bind failed");
+    }
+}
+
+void Acceptor::socketListen() {
+    assert(mListenFd != -1);
+    if (::listen(mListenFd, SOMAXCONN) == -1) {
+        errorif(true, "[Error]\t Socket Listen failed");
+    }
+}
+
+FLAG Acceptor::acceptConnection() const {
+    assert(mListenFd != -1);
+
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+
+    int clientFd = ::accept4(mListenFd, (struct sockaddr *)&client, &len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+
+    errorif(clientFd == -1, "[Error]\t Accept failed");
+
+    if (mNewConnectionCallback)
+        mNewConnectionCallback(clientFd);
+
+    return FL_SUCCESS;
+}
+
+void Acceptor::setNewConnectionCallback(std::function<void(int)> const &callback) {
+    // 设置新的连接回调函数
+    mNewConnectionCallback = std::move(callback);
+}
---
+++ Day15/src/include/Acceptor.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "Common.h"
+#include <functional>
+#include <memory>
+
+class Acceptor {
+public:
+    // 禁用拷贝和移动
+    DISALLOW_COPY_AND_MOVE(Acceptor);
+
+    explicit Acceptor(EventLoop *loop, std::string const &ip, int const port);
+    ~Acceptor();
+
+    /**
+     * @brief 接受连接
+     * @return 连接的情况
+     */
+    FLAG acceptConnection() const;
+
+    /**
+     * @brief 设置新连接的回调函数。
+     * @param callback 为新连接设置的回调函数。
+     */
+    void setNewConnectionCallback(std::function<void(int)> const &callback);
+
+    /**
+     * @brief 创建Socket
+     */
+    void socketCreate();
+
+    /**
+     * @brief 绑定ip和端口到socket
+     * @param ip Ip地址
+     * @param port 端口
+     */
+    void socketBind(std::string const &ip, int const port);
+
+    /**
+     * @brief 监听socket
+     */
+    void socketListen();
+
+private:
+    EventLoop *mLoop;
+    int mListenFd;
+    std::unique_ptr<Channel> mChannel;
+
+    // 定义一个新建连接的回调函数
+    std::function<void(int)> mNewConnectionCallback;
+};
