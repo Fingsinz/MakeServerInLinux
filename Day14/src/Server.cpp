49f5b042252423fea79cd214cbf5d31952079712
Fingsinz <fingsinz@outlook.com>
2024-07-13 17:35:51 +0800

    updated Server 类

--- Day14/src/Server.cpp
+++ Day14/src/Server.cpp
@@ -1,78 +1,77 @@
-﻿#include "Acceptor.h"
-#include "EventLoop.h"
-#include "Server.h"
-#include "Socket.h"
+﻿#include "Server.h"
+#include "Acceptor.h"
 #include "Connection.h"
+#include "EventLoop.h"
+#include "Socket.h"
 #include "ThreadPool.h"
 #include "util.h"
+#include <cassert>
 #include <functional>
+#include <iostream>
 #include <unistd.h>
 
 constexpr int READ_BUFFER = 1024;
 
-Server::Server()
-{
-	mainReactor = std::make_unique<EventLoop>();
-	acceptor = std::make_unique<Acceptor>(mainReactor.get());
-	std::function<void(int)> cb = std::bind(&Server::newConnection, this, std::placeholders::_1);
-	acceptor->setNewConnectionCallback(cb);
+Server::Server(std::string const &ip, int const port)
+        : nextConnId(1) {
+    mainReactor = std::make_unique<EventLoop>();
+    acceptor = std::make_unique<Acceptor>(mainReactor.get(), ip, port);
+    std::function<void(int)> cb = std::bind(&Server::handleNewConnection, this, std::placeholders::_1);
+    acceptor->setNewConnectionCallback(cb);
 
-	unsigned int size = std::thread::hardware_concurrency();
-	threadPool = std::make_unique<ThreadPool>(size);
+    unsigned int size = std::thread::hardware_concurrency();
+    threadPool = std::make_unique<ThreadPool>(size);
 
-	for (size_t i = 0; i < size; ++ i)
-	{
-		std::unique_ptr<EventLoop> subReactor = std::make_unique<EventLoop>();
-		subReactors.emplace_back(std::move(subReactor));
-	}
+    for (size_t i = 0; i < size; ++i) {
+        std::unique_ptr<EventLoop> subReactor = std::make_unique<EventLoop>();
+        subReactors.emplace_back(std::move(subReactor));
+    }
 }
 
-Server::~Server()
-{}
+Server::~Server() {}
 
-void Server::start()
-{
-	for (size_t i = 0; i < subReactors.size(); ++ i)
-	{
-		std::function<void()> subLoop = std::bind(&EventLoop::loop, subReactors[i].get());
-		threadPool->add(std::move(subLoop));
-	}
-	mainReactor->loop();
+void Server::start() {
+    for (size_t i = 0; i < subReactors.size(); ++i) {
+        std::function<void()> subLoop = std::bind(&EventLoop::loop, subReactors[i].get());
+        threadPool->add(subLoop);
+    }
+    mainReactor->loop();
 }
 
-void Server::onConnect(std::function<void(Connection *)> fn)
-{
-	onConnectionCallback = std::move(fn);
+void Server::setConnectionCallback(std::function<void(Connection *)> const &cb) {
+    onConnectCallback = std::move(cb);
 }
 
-void Server::onMessage(std::function<void(Connection *)> fn)
-{
-	onMessageCallback = std::move(fn);
+void Server::setMessageCallback(std::function<void(Connection *)> const &cb) {
+    onMessageCallback = std::move(cb);
 }
 
-FLAG Server::newConnection(int fd)
-{
-	errorif(fd == -1, "[Error]\t New Connection error");
-	uint64_t random = fd % subReactors.size();
-	std::unique_ptr<Connection> conn = std::make_unique<Connection>(subReactors[random].get(), fd);
-	std::function<void(int)> cb = std::bind(&Server::deleteConnection, this, std::placeholders::_1);
+void Server::handleNewConnection(int fd) {
+    assert(fd != -1);
+    std::cout << "[INFO]\tNew Connection, fd: " << fd << std::endl;
 
-	conn->setDeleteConnectionCallback(cb);
-	conn->setOnMessageCallback(onMessageCallback);
+    uint64_t random = fd % subReactors.size();
 
-	connections[fd] = std::move(conn);
+    Connection *conn = new Connection(subReactors[random].get(), fd, nextConnId);
+    std::function<void(int)> cb = std::bind(&Server::handleClose, this, std::placeholders::_1);
 
-	if (onConnectionCallback)
-		onConnectionCallback(connections[fd].get());
-	return FL_SUCCESS;
+    conn->setOnCloseCallback(cb);
+    conn->setOnMessageCallback(onMessageCallback);
+
+    connections[fd] = conn;
+
+    ++nextConnId;
+    if (nextConnId == 1000) {
+        nextConnId = 1;
+    }
 }
 
-FLAG Server::deleteConnection(int fd)
-{
+void Server::handleClose(int fd) {
 	auto it = connections.find(fd);
-	if (it != connections.end())
-	{
-		connections.erase(fd);
+	if (it != connections.end()) {
+		// delete it->second;
+		// 没有析构，所以在这里进行了 close，先关闭连接
+		::close(fd);
+		connections.erase(it);
 	}
-	return FL_SUCCESS;
 }
--- Day14/src/include/Server.h
+++ Day14/src/include/Server.h
@@ -12,53 +12,51 @@ class Connection;
 class Socket;
 class ThreadPool;
 
-class Server
-{
-private:
-	std::unique_ptr<EventLoop> mainReactor;					// 只负责接受连接，然后分发给一个subReactor
-	std::vector<std::unique_ptr<EventLoop>> subReactors;	// 处理事件循环
-	std::unique_ptr<Acceptor> acceptor;						// 连接接受器
-	std::unordered_map<int, std::unique_ptr<Connection>> connections;	// 存储连接及其相应的文件描述符
-	std::unique_ptr<ThreadPool> threadPool;					// 线程池
-
-	std::function<void(Connection *)> onConnectionCallback;	// 连接的业务逻辑
-	std::function<void(Connection *)> onMessageCallback;	// 接收消息后的业务逻辑
-
+class Server {
 public:
-	Server();
-	~Server();
+    Server(std::string const &ip, int const port);
+    ~Server();
 
-	// 禁止拷贝和移动
-	DISALLOW_COPY_AND_MOVE(Server);
+    // 禁止拷贝和移动
+    DISALLOW_COPY_AND_MOVE(Server);
 
-	/**
-	 * @brief 启动服务器
-	 */
-	void start();
+    /**
+     * @brief 启动服务器
+     */
+    void start();
 
-	/**
-	 * @brief 设置新连接建立时的回调
-	 * @param fn 回调函数
-	 */
-	void onConnect(std::function<void(Connection *)> fn);
+    /**
+     * @brief 设置连接回调函数
+     * @param cb 回调函数，接受一个Connection指针作为参数，无返回值。
+     */
+    void setConnectionCallback(std::function<void(Connection *)> const &cb);
 
-	/**
-	 * @brief 设置接收消息时的回调
-	 * @param fn 回调函数
-	 */
-	void onMessage(std::function<void(Connection *)> fn);
+    /**
+     * @brief 设置消息回调函数
+     * @param cb 回调函数，接受一个Connection指针作为参数，无返回值。
+     */
+    void setMessageCallback(std::function<void(Connection *)> const &cb);
 
-	/**
-	 * @brief 处理与给定文件描述符的新连接
-	 * @param fd 新连接的文件描述符
-	 * @return Flag 表示操作结果
-	 */
-	FLAG newConnection(int fd);
+    /**
+     * @brief 处理新的连接
+     * @param fd 新连接的文件描述符。
+     */
+    void handleNewConnection(int fd);
 
-	/**
-	 * @brief 删除与给定文件描述符的连接
-	 * @param fd 要删除的连接的文件描述符
-	 * @return Flag 表示操作结果
-	 */
-	FLAG deleteConnection(int fd);
+    /**
+     * @brief 关闭连接
+     * @param fd 需要关闭的连接的文件描述符。
+     */
+    void handleClose(int fd);
+
+private:
+    std::unique_ptr<EventLoop> mainReactor;              // 只负责接受连接，然后分发给一个subReactor
+    std::vector<std::unique_ptr<EventLoop>> subReactors; // 处理事件循环
+    std::unique_ptr<Acceptor> acceptor;                  // 连接接受器
+    int nextConnId;                                      // 下一个连接的ID
+    std::unordered_map<int, Connection *> connections;   // 存储连接及其相应的文件描述符
+    std::unique_ptr<ThreadPool> threadPool;              // 线程池
+
+    std::function<void(Connection *)> onConnectCallback; // 连接的业务逻辑
+    std::function<void(Connection *)> onMessageCallback; // 接收消息后的业务逻辑
 };
