d3fa94277fa3c04a240df49e75b1205de8799389
Fingsinz <fingsinz@outlook.com>
2024-07-17 15:15:01 +0800

    update 代码格式及修改

---
+++ Day14/build/test/CMakeFiles/MultipleClients.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultipleClients.dir/MultipleClients.cpp.o"
+  "../bin/MultipleClients.pdb"
+  "../bin/MultipleClients"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MultipleClients.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Day14/build/test/CMakeFiles/SingleClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleClient.dir/SingleClient.cpp.o"
+  "../bin/SingleClient.pdb"
+  "../bin/SingleClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SingleClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
